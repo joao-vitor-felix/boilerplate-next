b8b3cf7f987f51ceacf0dd0a797a0b189abbbf0b
João Vitor <joaovitorfelixcontato@gmail.com>
2024-03-29 18:14:19 -0300

    chore: add path prompt to plopfile

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -6,21 +6,26 @@ module.exports = function (plop) {
     prompts: [
       {
         type: "input",
+        name: "path",
+        message: "component path?",
+      },
+      {
+        type: "input",
         name: "name",
         message: "component name?"
-      }
+      },
     ],
 
     // actions to perform
     actions: [
       {
         type: "add",
-        path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
+        path: "../src/{{path}}/{{pascalCase name}}/{{pascalCase name}}.tsx",
         templateFile: "templates/component.tsx.hbs"
       },
       {
         type: "add",
-        path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx",
+        path: "../src/{{path}}/{{pascalCase name}}/{{pascalCase name}}.test.tsx",
         templateFile: "templates/component.test.tsx.hbs"
       }
     ]
